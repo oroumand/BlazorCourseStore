3c253e1114aa57c75a1bd1383082dfd96cc3847b
a.oroumand <a.oroumand@dotin.ir>
2024-01-09 15:01:48 +0330

    feat: config teacher aggregate in ef core

--- src/2.Infra/Data/CourseStore.Infra.Data.Sql.Commands/Common/CourseStoreCommandDbContext.cs
+++ src/2.Infra/Data/CourseStore.Infra.Data.Sql.Commands/Common/CourseStoreCommandDbContext.cs
@@ -1,7 +1,11 @@
 ﻿using CourseStore.Core.Domain.Teachers.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Reflection;
+using Zamin.Core.Domain.Toolkits.ValueObjects;
+using Zamin.Core.Domain.ValueObjects;
 using Zamin.Extensions.Events.Outbox.Dal.EF;
+using Zamin.Infra.Data.Sql.Commands.ValueConversions;
 
 namespace CourseStore.Infra.Data.Sql.Commands.Common;
 
@@ -18,5 +22,13 @@ public class CourseStoreCommandDbContext : BaseOutboxCommandDbContext
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(builder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<BusinessId>().HaveConversion<BusinessIdConversion>();
+        configurationBuilder.Properties<Title>().HaveConversion<TitleConversion>();
+        configurationBuilder.Properties<Description>().HaveConversion<DescriptionConversion>();
+        configurationBuilder.Properties<NationalCode>().HaveConversion<NationalCodeConversion>();
     }
 }
---
+++ src/2.Infra/Data/CourseStore.Infra.Data.Sql.Commands/Teachers/Configs/TeacherConfig.cs
@@ -0,0 +1,21 @@
+﻿using CourseStore.Core.Domain.Teachers.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseStore.Infra.Data.Sql.Commands.Teachers.Configs
+{
+    internal class TeacherConfig : IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.Property(c => c.Id);
+            builder.Property(c => c.BusinessId).IsRequired();
+            builder.HasIndex(c => c.BusinessId).IsUnique();
+            builder.Property(c => c.FirstName).HasMaxLength(250);
+            builder.Property(c => c.LastName).HasMaxLength(250);
+            builder.Property(c => c.Description).HasMaxLength(500);
+            builder.Property(c => c.ImageUrl).HasMaxLength(1000);
+
+        }
+    }
+}
