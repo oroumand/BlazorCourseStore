88fe84b7d2d533e006e9423e298dda87e4f746c5
a.oroumand <a.oroumand@dotin.ir>
2024-01-09 15:51:45 +0330

    feat: implement Rest API for creating teacher

--- src/1.Core/CourseStore.Core.RequestResponse/Teachers/Command/CreateTeacherCommand.cs
+++ src/1.Core/CourseStore.Core.RequestResponse/Teachers/Command/CreateTeacherCommand.cs
@@ -2,6 +2,11 @@
 
 namespace CourseStore.Core.RequestResponse.Teachers.Command
 {
-    public record CreateTeacherCommand(string FirstName,string LastName,string Description, string ImageUrl): ICommand<int>;
-
+    public class CreateTeacherCommand:ICommand<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+}
 }
---
+++ src/3.Endpoints/CourseStore.Endpoints.API/Teachers/TeachersController.cs
@@ -0,0 +1,24 @@
+﻿using CourseStore.Core.RequestResponse.Teachers.Command;
+using Microsoft.AspNetCore.Mvc;
+using Zamin.EndPoints.Web.Controllers;
+
+namespace SSO.Management.Endpoints.API.Applications;
+
+[Route("api/[controller]")]
+public class TeachersController : BaseController
+{
+    #region Commands
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Create([FromBody] CreateTeacherCommand command)
+        => await Create<CreateTeacherCommand,int>(command);
+
+
+    #endregion
+
+    #region Queries
+
+    
+
+    #endregion
+}
