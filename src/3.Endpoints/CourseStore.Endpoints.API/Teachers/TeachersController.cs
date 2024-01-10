905f2e703a30f4e1e0e0acb7e7c97fd4bbaab5e3
a.oroumand <a.oroumand@dotin.ir>
2024-01-10 10:37:40 +0330

    feat: Compelete Teachers CRUD  REST api

--- src/3.Endpoints/CourseStore.Endpoints.API/Teachers/TeachersController.cs
+++ src/3.Endpoints/CourseStore.Endpoints.API/Teachers/TeachersController.cs
@@ -1,4 +1,7 @@
 ﻿using CourseStore.Core.RequestResponse.Teachers.Command.CreateTeachers;
+using CourseStore.Core.RequestResponse.Teachers.Command.RenameTeachers;
+using CourseStore.Core.RequestResponse.Teachers.Command.UpdateDescription;
+using CourseStore.Core.RequestResponse.Teachers.Command.UpdateImageUrl;
 using Microsoft.AspNetCore.Mvc;
 using Zamin.EndPoints.Web.Controllers;
 
@@ -13,12 +16,23 @@ public class TeachersController : BaseController
     public async Task<IActionResult> Create([FromBody] CreateTeacherCommand command)
         => await Create<CreateTeacherCommand,int>(command);
 
+    [HttpPut("[action]")]
+    public async Task<IActionResult> Rename([FromBody] RenameTeacherCommand command)
+        => await Edit(command);
 
+    [HttpPut("[action]")]
+    public async Task<IActionResult> UpdateDescription([FromBody] UpdateDescriptionCommand command)
+        => await Edit(command);
+
+
+    [HttpPut("[action]")]
+    public async Task<IActionResult> UpdateImage([FromBody] UpdateImageCommand command)
+        => await Edit(command);
     #endregion
 
     #region Queries
 
-    
+
 
     #endregion
 }
